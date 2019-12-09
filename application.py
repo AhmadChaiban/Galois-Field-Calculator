945f6a61c0e8e0b6f4f0aa35191408e15b90309c
Ahmad Chaiban <ahmadchaiban@gmail.com>
2019-12-09 09:22:05 +0200

    GUI Overhaul

--- application.py
+++ application.py
@@ -13,9 +13,6 @@ from pyfinite import ffield
 ##     5. Whether to show the result as a polynomial or binary (boolean)
 
 def ffield_calculator(polyList,operation,field_power_m,generator,showPoly,showHex): 
-    ## If there are no values in the list of binary numbers, return an error
-    if len(polyList) <= 0:
-        return "Error: Please enter binary values to compute"
     ## if the field generator is 0 and greater than 100, return an error
     if generator == 0 and field_power_m>100:
         return 'generator cannot be 0'
@@ -69,9 +66,6 @@ def ffield_calculator(polyList,operation,field_power_m,generator,showPoly,showHe
             c = F.MultiplyWithoutReducing(c,a)
         ## if the operation selected is mod, then:
         elif operation == 'mod':
-            ## If the operation is mod and there is only one element, throw an error
-            if len(polyList) == 1:
-                return "Error, enter two values for this operation"
             ## Else if there are exactly two elements, apply the full division
             c = F.FullDivision(c,a,F.FindDegree(c),F.FindDegree(a))
     ## if the operation is mod then:
@@ -90,75 +84,105 @@ def ffield_calculator(polyList,operation,field_power_m,generator,showPoly,showHe
     elif showHex == True:
         return hex(c)
     return ('{0:b}'.format(c))
-from tkinter import *
 
 ##########################################################################################
                                     ## GUI using tkinter
 ##########################################################################################
                                   
+from tkinter import *
+from PIL import Image
 
 ##########################################################################################
 master = Tk() ##initialize the GUI
 ##########################################################################################
 ## Rename the title of the software
 master.title('Galois Field Calculator')
+## setting geometry for GUI
+master.geometry('800x480')
+##Using Pillow library to import image
+backImg = Image.open('UI_back.png')
+backImg = backImg.resize((800,480),Image.ANTIALIAS)
+backImg.save('UI.ppm','ppm')
+gridImg = Image.open('grid.jpg')
+gridImg = gridImg.resize((400,100),Image.ANTIALIAS)
+gridImg.putalpha(128)
+gridImg.save('grid.ppm','ppm')
+## selecting background image
+backImg = PhotoImage(file = 'UI.ppm')
+label_for_image = Label(master,image=backImg,border=0)
+label_for_image.grid(row = 0)
+## selecting the grid 
+gridImg = PhotoImage(file = 'grid.ppm')
+label_for_grid = Label(master,image=gridImg,border=2,bg='#56E29B')
+label_for_grid.place(x=270,y=140)
 ## Set the background of the GUI as black
 master.configure(background = 'black')
 ## Define the labels for each input box
 ## This is the first input box for the field power 
-Label(master, text='Field GF(2^m), type in the power m',font=('monospace',8),bg='black',fg='#7D5710').grid(row=0) 
+Label(master, text='Field GF(2^m), type in the power m',font=('monospace',8),bg='black',fg='#56E29B').place(x=300,y=250) 
 fieldVal = Entry(master,bg='#6E7B7C')
-fieldVal.grid(row=0,column=1)
+fieldVal.place(x=500,y=250)
 ##Defining hte second input box for the field generator value
-Label(master, text='Type in the Generator value',font=('monospace',8),bg='black',fg='#7D5710').grid(row=1) 
+Label(master, text='Type in the Generator value',font=('monospace',8),bg='black',fg='#56E29B').place(x=300,y=280)
 genVal = Entry(master,bg='#6E7B7C')
-genVal.grid(row=1,column=1)
+genVal.place(x=500,y=280)
 ## Input box to enter the binary numbers required for operation
-Label(master, text='Values for operation (split with spaces)',font = ('monospace',8),bg='black',fg='#7D5710').grid(row=2) 
+Label(master, text='Values for operation (split with spaces)',font = ('monospace',8),bg='black',fg='#56E29B').place(x=300,y=310)
 polyStr = Entry(master,bg='#6E7B7C')
-polyStr.grid(row=2,column=1)
+polyStr.place(x=500,y=310)
 ## Define the checkbutton variable as a boolean variable
 showPoly = BooleanVar()
 ## Define the actual checkbutton here, style it and assign it to the GUI grid
-Checkbutton(master, text='Polynomial Form (Default is Binary, only for result)', variable=showPoly,font=('monospace',8),bg='black',fg='#029FC6').grid(row=3,columnspan = 2)
+Checkbutton(master, text='Polynomial Form', variable=showPoly,font=('monospace',8),bg='black',fg='#029FC6').place(x=330,y = 410)
 ## Define the checkbutton variable as a boolean variable
 showHex = BooleanVar()
 ## Define the actual checkbutton here, style it and assign it to the GUI grid
-Checkbutton(master, text='Hexadecimal form (Default is Binary, for input and output)', variable=showHex,font=('monospace',8),bg='black',fg='#029FC6').grid(row=4,columnspan = 2)
+Checkbutton(master, text='Hexadecimal form', variable=showHex,font=('monospace',8),bg='black',fg='#029FC6').place(x=330,y=430)
 ## Define the label for results of the calculations globally 
 global label
 ## The label here is being defined and set as empty for updating per iteration purposes
-label = Label(master, textvariable= "", font = ('monospace',20),bg='black',fg='#029FC6')
+label = Label(master, textvariable= "", font = ('monospace',10),bg='black',fg='#029FC6')
 ## The following function prints the result of the button press
 def printResult(oper):
     ## Splitting the binary number input string
     polys = polyStr.get().split(" ")
     ## Calculating the values
+    ## If there are no values in the list of binary numbers, return an error
+    if len(polys) == 0 or fieldVal.get() == '' or genVal.get() == '' :
+        label['text'] = "Error: Please enter field/values to compute"
+        label.place(x=290,y=180)
+        return 0
+    ## if values entered for mod are only 1, throw an error
+    if oper == 'mod' and len(polys) == 1:
+        label['text'] = "Error: Please enter exactly two elements"
+        label.place(x=290,y=180)
+        return 0
+    ## make the calculations in the selected field
     x = ffield_calculator(polys,oper,int(fieldVal.get()),int(genVal.get()),showPoly.get(),showHex.get())
     ## Changing the label value to the result
     label['text'] = str(x)
     ## Assigning the label to the GUI grid
-    label.grid(row = 9,column =0,columnspan=2)
+    label.place(x=290,y=180)
     
 ## Defining each operation button an assigning it in the GUI grid. Each button runs the printResult function with the corresponding operation
 ## The function implementation was done using lambda notation
 ## Some styling was also applied to each button.
-Button1 = Button(master, text='Multiply',font=('monospace',8), width=30, command= lambda: printResult('multiply'),fg='white',bg='#262626')
-Button1.grid(row=5)
-Button2 = Button(master, text='Divide', font=('monospace',8),width=30, command= lambda: printResult('divide'),fg='white',bg='#262626')
-Button2.grid(row=5,column=1)
-Button3 = Button(master, text='Add',font=('monospace',8), width=30, command= lambda: printResult('add'),fg='white',bg='#262626')
-Button3.grid(row=6,column=0)
-Button4 = Button(master, text='Subtract',font=('monospace',8), width=30, command= lambda: printResult('subtract'),fg='white',bg='#262626')
-Button4.grid(row=6,column=1)
-Button5 = Button(master, text='Inverse',font=('monospace',8), width=30, command= lambda: printResult('inverse'),fg='white',bg='#262626')
-Button5.grid(row=7,column=0)
-Button6 = Button(master, text='Modulo', font=('monospace',8), width=30, command= lambda: printResult('mod'),fg='white',bg='#262626')
-Button6.grid(row=7,column=1)
-Button62 = Button(master, text='Multiply without reducing',font=('monospace',8), width=52, command= lambda: printResult('mod'),fg='white',bg='#262626')
-Button62.grid(row=8,column=0,columnspan=2)
-Button7 = Button(master, text='Exit',font=('monospace',8), width=52, command= master.destroy,fg='#7D5710',bg='#262626')
-Button7.grid(row=11,columnspan=2)
+Button1 = Button(master, text='Multiply',font=('monospace',10), width=0, command= lambda: printResult('multiply'),fg='#56E29B',bg='black',border=0)
+Button1.place(x = 80,y = 27)
+Button2 = Button(master, text='Divide', font=('monospace',10),width=0, command= lambda: printResult('divide'),fg='#56E29B',bg='black',border=0)
+Button2.place(x = 80,y = 75)
+Button3 = Button(master, text='Add',font=('monospace',10), width=0, command= lambda: printResult('add'),fg='#56E29B',bg='black',border=0)
+Button3.place(x = 80,y = 120)
+Button4 = Button(master, text='Subtract',font=('monospace',10), width=0, command= lambda: printResult('subtract'),fg='#56E29B',bg='black',border=0)
+Button4.place(x = 80,y = 165)
+Button5 = Button(master, text='Inverse',font=('monospace',10), width=0, command= lambda: printResult('inverse'),fg='#56E29B',bg='black',border=0)
+Button5.place(x = 80,y = 211)
+Button6 = Button(master, text='Modulo', font=('monospace',10), width=0, command= lambda: printResult('mod'),fg='#56E29B',bg='black',border=0)
+Button6.place(x = 80,y = 258)
+Button62 = Button(master, text='Multiply w/ reducing',font=('monospace',10), width=0, command= lambda: printResult('mod'),fg='#56E29B',bg='black',border=0)
+Button62.place(x = 80,y = 306)
+Button7 = Button(master, text='Exit',font=('monospace',10), width=0, command= master.destroy,fg='#56E29B',bg='black',border=0)
+Button7.place(x = 80,y = 352)
 
 ############################################################################################
 mainloop() ## loop the GUI
